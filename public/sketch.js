182965e1837a5c965d57e2326d512f567fc0cd11
spenielle <denielle.sambilay@sjsu.edu>
2026-05-20 20:05:58 -0700

    update

--- public/sketch.js
+++ public/sketch.js
@@ -4,7 +4,8 @@ function setup() {
     createCanvas(200, 200);
     background(51);
 
-    socket = io.connect('http://localhost:3000')
+    // socket = io.connect('http://localhost:3000')
+    socket = io();
     socket.on('mouse', newDrawing);
 
 }
